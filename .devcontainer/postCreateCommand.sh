1d423e75d32d14bf9e04a24b95cb96ba9eb563af
toriwasa <toriwasa.py@gmail.com>
2025-03-29 11:22:35 +0000

    uvをPATHに追加するコマンド追加

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,5 +6,8 @@ sudo chown -R vscode:vscode /workspace
 # pip の代わりにパッケージ管理ツール uv をインストールする
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+# uv のインストール先を PATH に追加する
+source /home/vscode/.local/bin/env
+
 # プロジェクトに必要な Python パッケージをインストールする
 uv sync
