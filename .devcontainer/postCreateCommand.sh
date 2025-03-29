a6fc7d7685f32d28d51d28b18eded9f25ef9171c
toriwasa <toriwasa.py@gmail.com>
2025-03-29 11:38:38 +0000

    仮想環境を3.11で作成する

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,5 +9,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 # uv のインストール先を PATH に追加する
 source /home/vscode/.local/bin/env
 
+# Python 3.11 の.venv 作成
+uv venv --python 3.11
+
 # プロジェクトに必要な Python パッケージをインストールする
 uv sync
